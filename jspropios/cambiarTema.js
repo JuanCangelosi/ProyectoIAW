1a52ff01fdc8085d846c0ee6e98ac8c4fed3c328
Juan <juani.cangelosi@gmail.com>
2017-04-19 11:59:10 -0300

    Agregada funcionalidad de guardado de estilo por cookies

--- jspropios/cambiarTema.js
+++ jspropios/cambiarTema.js
@@ -3,9 +3,16 @@ $("#tema").click(cambiarCSS);
 function cambiarCSS(e){
     var addressValue = $("#cssPropio").attr("href");
     if(addressValue.includes("1")){
-	   $('link[href="csspropios/bootstrap1.css"]').attr('href','csspropios/bootstrap2.css');
+        console.log("tiene 1");
+        setCSS("csspropios/bootstrap2.css");
     }
     else{
-        $('link[href="csspropios/bootstrap2.css"]').attr('href','csspropios/bootstrap1.css');
+        console.log("tiene 2");
+        setCSS("csspropios/bootstrap1.css");
     }
+}
+
+function setCSS(direccion){
+    $("#cssPropio").attr('href', direccion);
+    document.cookie = "css="+direccion+"; expires= Fri, 31 Dec 9999 23:59:59 GMT";
 }
